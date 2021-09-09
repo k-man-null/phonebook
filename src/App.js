45118abae1092108959755c62e07d6184a01d479
Bill Konchellah <williamoriginal2019@gmail.com>
2021-09-09 16:51:54 +0300

    "Added useeffect"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-
+import React, { useState , useEffect } from 'react'
+import axios from 'axios'
 
 const Form = (props) => {
   return (
@@ -25,17 +25,19 @@ const Form = (props) => {
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
 
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  },[])
   const handleName = (event) => {
     setNewName(event.target.value)
   }
